968e4ff48f1ec40cee37074e3a11749f5f2772dc
siva <siva.l@tesark.in>
2026-01-29 14:29:15 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import StudentListingPage from './components/studentListingPage'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>School Management System</h1>
+        <StudentListingPage />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/studentListingPage.tsx
@@ -0,0 +1,11 @@
+
+
+function StudentListingPage() {
+  return (
+    <div>
+        <h1>Student Listing Page</h1>
+    </div>
+  )
+}
+
+export default StudentListingPage
